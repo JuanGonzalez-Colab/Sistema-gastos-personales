6460280b16bd0c39cd6c1114285800e9739ad27a
JuanGonzalez-Colab <juantomyjenny@gmail.com>
2026-08-08 09:33:47 -0300

    Update app_gastos.py

--- app_gastos.py
+++ app_gastos.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyM8+3VwDv4ikVRlwUJ5QV0U",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
